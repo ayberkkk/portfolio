46dc0105e25d2505dd28f0c6c9594475a7ca6d77
ayberkkk <ayberktiglii@gmail.com>
2023-03-31 15:51:31 +0300

    FIX img - a rel

--- src/components/home/Socials.jsx
+++ src/components/home/Socials.jsx
@@ -6,21 +6,21 @@ const Socials = () => {
       <a
         href="https://www.instagram.com/aayberkt/"
         className="home__social-icon"
-        target="_blank"
+        target="_blank" rel="noreferrer"
       >
         <i class="uil uil-instagram"></i>
       </a>
       <a
         href="https://www.linkedin.com/in/ayberk-tigli/"
         className="home__social-icon"
-        target="_blank"
+        target="_blank" rel="noreferrer"
       >
         <i class="uil uil-linkedin-alt"></i>
       </a>
       <a
         href="https://github.com/ayberkkk"
         className="home__social-icon"
-        target="_blank"
+        target="_blank" rel="noreferrer"
       >
         <i class="uil uil-github-alt"></i>
       </a>
