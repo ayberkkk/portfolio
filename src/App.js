4ad49e62c5a516ea2b9c2a9e999c3b6ccc036073
ayberkkk <ayberktiglii@gmail.com>
2023-04-26 16:18:20 +0300

    DESING > project section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 import About from './components/about/About';
+import Projects from './components/projects/Projects'
 import Skills from './components/skills/Skills';
 import Services from './components/services/Services';
 import Qualification from './components/qualification/Qualification';
@@ -15,6 +16,7 @@ function App() {
       <main className='main'>
         <Home />
         <About />
+        <Projects />
         <Skills />
         <Services />
         <Qualification/>
